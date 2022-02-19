d0d5bad1c6f99334264cfabb34bfabbe2b621d20
Deepali Srivastava <deepalisriv9@gmail.com>
2022-02-19 17:41:07 +0530

    Address calculation sort in C#

---
+++ sorting/AddressCalculationSort/Program.cs
@@ -0,0 +1,76 @@
+﻿/*
+Copyright (C) Deepali Srivastava - All Rights Reserved
+This code is part of DSA course available on CourseGalaxy.com    
+*/
+
+using System;
+
+namespace AddressCalculationSort
+{
+    class Program
+    {
+        public static void Sort(int[] a, int n)
+        {
+            int i, j, x;
+
+            SortedLinkedList[] List = new SortedLinkedList[6];
+            for (i = 0; i < 6; i++)
+                List[i] = new SortedLinkedList();
+
+            int large = 0;
+            for (i = 0; i < n; i++)
+            {
+                if (a[i] > large)
+                    large = a[i];
+            }
+
+            for (i = 0; i < n; i++)
+            {
+                x = Hash(a[i], large);
+                List[x].InsertInOrder(a[i]);
+            }
+
+            /*Elements of linked lists are copied to array*/
+            Node p;
+            i = 0;
+            for (j = 0; j <= 5; j++)
+            {
+                p = List[j].GetStart();
+                while (p != null)
+                {
+                    a[i++] = p.info;
+                    p = p.link;
+                }
+            }
+        }
+
+        public static int Hash(int x, int large)
+        {
+            float temp;
+            temp = (float)x / large;
+            return (int)(temp * 5);
+        }
+	
+        static void Main(string[] args)
+        {
+            int i,n;
+		    int[] a = new int[20];
+		    		
+		    Console.Write("Enter the number of elements : ");
+		    n = Convert.ToInt32(Console.ReadLine());
+				
+		    for( i = 0; i < n; i++ )
+		    {
+			    Console.Write("Enter element " + (i+1) + " : ");
+			    a[i] = Convert.ToInt32(Console.ReadLine());
+		    }
+		
+		    Sort(a, n);
+		
+		    Console.WriteLine("Sorted array is : ");
+		    for( i = 0; i < n; i++ )
+			    Console.Write(a[i] + " ");
+		    Console.WriteLine();
+        }
+    }
+}
---
+++ sorting/AddressCalculationSort/SortedLinkedList.cs
@@ -0,0 +1,109 @@
+﻿/*
+Copyright (C) Deepali Srivastava - All Rights Reserved
+This code is part of DSA course available on CourseGalaxy.com    
+*/
+
+using System;
+
+namespace AddressCalculationSort
+{
+    class SortedLinkedList
+    {
+        private Node start;
+
+        public SortedLinkedList()
+        {
+            start = null;
+        }
+
+        public void InsertInOrder(int data)
+        {
+            Node p, temp;
+            temp = new Node(data);
+
+            /*List empty or new node to be inserted before first node*/
+            if (start == null || data < start.info)
+            {
+                temp.link = start;
+                start = temp;
+                return;
+            }
+
+            p = start;
+            while (p.link != null && p.link.info <= data)
+                p = p.link;
+
+            temp.link = p.link;
+            p.link = temp;
+
+        }
+
+        public void CreateList()
+	    {
+		    int i, n, data;
+		
+		    Console.Write("Enter the number of nodes : ");
+		    n = Convert.ToInt32(Console.ReadLine());
+		
+		    if(n == 0)
+			    return;
+		
+		    for( i = 1; i <= n; i++ )
+		    {
+			    Console.Write("Enter the element to be inserted : ");
+			    data = Convert.ToInt32(Console.ReadLine());
+			    InsertInOrder(data);
+		    }
+	    }
+
+        public void Search(int x)
+	    {
+		    Node p;
+		    int position;
+		
+		    if( start == null )
+		    {
+			    Console.WriteLine("List is empty\n");
+			    return;
+		    }
+
+		    p = start;
+		    position = 1;
+		    while( p != null && p.info <= x )
+		    {
+			    if( p.info == x )
+				    break;
+			    position++;
+			    p = p.link;
+		    }
+		    
+            if(p == null || p.info != x )
+			    Console.WriteLine(x + " not found in list");
+		    else
+			    Console.WriteLine(x + " is at position " + position);	
+	    }
+
+        public void DisplayList()
+	    {
+		    Node p;
+		    if( start == null )
+		    {
+			    Console.WriteLine("List is empty");
+			    return;
+		    }
+		    Console.Write("List is :   ");
+		    p = start;
+		    while(p != null)
+		    {
+			    Console.Write(p.info + "  ");
+			    p = p.link;
+		    }
+		    Console.WriteLine();
+	    }
+
+        public Node GetStart()
+        {
+            return start;
+        }
+    }
+}
