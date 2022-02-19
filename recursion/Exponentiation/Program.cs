a2aedf78382c4a4601f401ccdad42aeafdac6055
Deepali Srivastava <deepalisriv9@gmail.com>
2022-02-19 17:56:25 +0530

    Exponentiation in C#

---
+++ recursion/Exponentiation/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+Copyright (C) Deepali Srivastava - All Rights Reserved
+This code is part of DSA course available on CourseGalaxy.com    
+*/
+
+using System;
+
+namespace Exponentiation
+{
+    class Program
+    {
+        public static double Power(double x, int n)
+        {
+            if (n == 0)
+                return 1;
+            return x * Power(x, n - 1);
+        }
+
+        static void Main(string[] args)
+        {
+            double x;
+		    int n;
+		
+		    Console.Write("Enter values for x and n : ");
+		   
+            x = Convert.ToDouble(Console.ReadLine());
+		
+            n = Convert.ToInt32(Console.ReadLine());
+		
+		    Console.WriteLine(x + "^" + n + "=" + Power(x,n));
+        }
+    }
+}
