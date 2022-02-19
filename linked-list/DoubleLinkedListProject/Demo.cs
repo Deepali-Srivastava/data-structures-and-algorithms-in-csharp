96024ec9957ccc70dd709de1d1b011e8d9359253
Deepali Srivastava <deepalisriv9@gmail.com>
2022-02-19 17:37:38 +0530

    Doubly linked list in C#

---
+++ linked-list/DoubleLinkedListProject/Demo.cs
@@ -0,0 +1,95 @@
+﻿/*
+Copyright (C) Deepali Srivastava - All Rights Reserved
+This code is part of DSA course available on CourseGalaxy.com    
+*/
+
+using System;
+
+namespace DoubleLinkedListProject
+{
+    class Demo
+    {
+        static void Main(string[] args)
+        {
+            int choice, data, x;
+		    
+            DoubleLinkedList list = new DoubleLinkedList();
+		    list.CreateList();
+
+		    while ( true )
+		    {
+			    Console.WriteLine("1.Display List");
+			    Console.WriteLine("2.Insert in empty list");
+			    Console.WriteLine("3.Insert a node in beginning of the list");
+			    Console.WriteLine("4.Insert a node at the end of the list");
+			    Console.WriteLine("5.Insert a node after a specified node");
+			    Console.WriteLine("6.Insert a node before a specified node");
+			    Console.WriteLine("7.Delete first node");
+			    Console.WriteLine("8.Delete last node");
+			    Console.WriteLine("9.Delete any node");
+			    Console.WriteLine("10.Reverse the list");
+			    Console.WriteLine("11.Quit");
+			    Console.WriteLine("Enter your choice : ");
+			    choice = Convert.ToInt32(Console.ReadLine());
+
+			    if ( choice == 11 )
+				    break;
+
+			    switch ( choice )
+			    {
+			     case 1:
+				    list.DisplayList();
+				    break;
+			     case 2:
+				    Console.Write("Enter the element to be inserted : ");
+				    data = Convert.ToInt32(Console.ReadLine());
+				    list.InsertInEmptyList(data);
+				    break;
+			     case 3:
+		            Console.Write("Enter the element to be inserted : ");
+		            data = Convert.ToInt32(Console.ReadLine());
+				    list.InsertInBeginning(data);
+				    break;
+			     case 4: 
+				    Console.Write("Enter the element to be inserted : ");
+				    data = Convert.ToInt32(Console.ReadLine());
+				    list.InsertAtEnd(data);
+				    break;
+			     case 5:
+				    Console.Write("Enter the element to be inserted : ");
+				    data = Convert.ToInt32(Console.ReadLine());
+				    Console.Write("Enter the element after which to insert : ");
+				    x = Convert.ToInt32(Console.ReadLine());
+				    list.InsertAfter(data,x);
+				    break;
+			     case 6:
+				    Console.Write("Enter the element to be inserted : ");
+				    data = Convert.ToInt32(Console.ReadLine());
+				    Console.Write("Enter the element before which to insert : ");
+				    x = Convert.ToInt32(Console.ReadLine());
+				    list.InsertBefore(data,x);
+				    break;
+			     case 7:
+				    list.DeleteFirstNode();
+			   	    break;
+			     case 8:
+				    list.DeleteLastNode();
+				    break;
+			     case 9:
+				    Console.Write("Enter the element to be deleted : ");
+				    data = Convert.ToInt32(Console.ReadLine());
+				    list.DeleteNode(data);
+		   		    break;
+			     case 10:
+				    list.ReverseList();
+				    break;
+			     default:
+				    Console.WriteLine("Wrong choice");
+                    break;
+		    }
+		    Console.WriteLine();	
+	       }
+	       Console.WriteLine("Exiting");
+	    }
+    }
+}
---
+++ linked-list/DoubleLinkedListProject/DoubleLinkedList.cs
@@ -0,0 +1,236 @@
+﻿/*
+Copyright (C) Deepali Srivastava - All Rights Reserved
+This code is part of DSA course available on CourseGalaxy.com    
+*/
+
+using System;
+
+namespace DoubleLinkedListProject
+{
+    class DoubleLinkedList
+    {
+        private Node start;
+
+        public DoubleLinkedList()
+        {
+            start = null;
+        }
+        
+        public void DisplayList()
+	    {
+		    Node p;
+		    if ( start == null )
+		    {
+			    Console.WriteLine("List is empty");
+			    return;
+		    }
+		
+		    p = start;
+		    Console.Write("List is : ");
+		    while ( p != null )
+		    {
+			    Console.Write(p.info + " ");
+			    p = p.next;
+		    }
+		    Console.WriteLine();
+	    }
+
+        public void InsertInBeginning(int data)
+        {
+            Node temp = new Node(data);
+            temp.next = start;
+            start.prev = temp;
+            start = temp;
+        }
+
+        public void InsertInEmptyList(int data)
+        {
+            Node temp = new Node(data);
+            start = temp;
+        }
+
+        public void InsertAtEnd(int data)
+        {
+            Node temp = new Node(data);
+
+            Node p = start;
+            while (p.next != null )
+                p = p.next;
+            
+            p.next = temp;
+            temp.prev = p;
+        }
+
+        public void CreateList()
+	    {
+		    int i,n,data;
+			Console.Write("Enter the number of nodes : ");
+		    n = Convert.ToInt32(Console.ReadLine());
+				
+		    if(n == 0)
+			    return;
+				
+		    Console.Write("Enter the first element to be inserted : ");
+		    data = Convert.ToInt32(Console.ReadLine());
+		    InsertInEmptyList(data);
+			
+		    for ( i = 2; i <= n; i++ )
+		    {
+			    Console.Write("Enter the next element to be inserted : ");
+			    data = Convert.ToInt32(Console.ReadLine());
+			    InsertAtEnd(data);	
+		    }
+	    }
+
+        public void InsertAfter(int data, int x)
+	    {
+		    Node temp = new Node(data);
+		    
+            Node p = start;
+            while ( p != null )
+		    {
+			    if ( p.info == x )
+				    break;
+			    p = p.next;
+		    }
+		
+	    	if(p == null)
+			    Console.WriteLine(x + " not present in the list");
+		    else
+		    {
+			    temp.prev = p;
+			    temp.next = p.next;
+			    if ( p.next != null )   
+				    p.next.prev = temp; /*should not be done when p refers to last node*/
+			    p.next = temp;
+		    }
+    	}
+        
+        public void InsertBefore(int data, int x)
+	    {
+		    if ( start == null )
+		    {
+			    Console.WriteLine("List is empty");
+			    return;
+		    }
+		    if ( start.info == x )
+		    {
+			    Node temp = new Node(data);
+			    temp.next = start;
+			    start.prev = temp;
+			    start = temp;
+			    return;
+		    }
+		
+		    Node p = start;
+		    while ( p != null )
+		    {
+			    if( p.info == x )
+				    break;
+			    p = p.next;
+		    }
+		
+		    if ( p == null )
+			    Console.WriteLine(x + " not present in the list");
+		    else
+		    {
+			        Node temp = new Node(data);
+				    temp.prev = p.prev;
+				    temp.next = p;
+				    p.prev.next = temp;
+				    p.prev = temp;
+		    }
+	    }
+
+        public void DeleteFirstNode()
+        {
+            if ( start == null )  /*list is empty*/
+                return;
+            if ( start.next == null ) /*list has only one node*/
+            {
+                start = null;
+                return;
+            }
+            start = start.next;
+            start.prev = null;
+        }
+
+        public void DeleteLastNode()
+        {
+            if (start == null)   /*list is empty*/
+                return;
+            if (start.next == null)  /*list has only one node*/
+            {
+                start = null;
+                return;
+            }
+            Node p = start;
+            while (p.next != null)
+                p = p.next;
+            p.prev.next = null;
+        }
+
+        public void DeleteNode(int x)
+	    {
+		    if( start == null )  /*list is empty*/
+			    return;
+		
+		    if ( start.next == null )	/*list has only one node*/
+		    {
+			    if ( start.info == x ) 
+				    start = null;
+			    else
+				    Console.WriteLine(x + " not found");
+			    return;
+		    }
+		
+		    /*Deletion of first node*/
+		    if ( start.info == x )
+		    {
+		 	    start = start.next;  
+			    start.prev = null;
+			    return;
+		    }
+		
+		    Node p = start.next;
+		    while ( p.next != null )  
+		    {
+			    if ( p.info == x )     
+			      break;
+			    p = p.next;
+		    }
+		
+		    if ( p.next != null ) /*node to be deleted is in between*/
+		    {
+			    p.prev.next = p.next;
+			    p.next.prev = p.prev;
+		    }
+		    else /*p refers to last node*/
+		    {
+			    if ( p.info == x )/*node to be deleted is last node*/
+				    p.prev.next = null;
+			    else 
+				    Console.WriteLine(x + " not found");
+		    }
+	    }
+
+        public void ReverseList()
+        {
+            if (start == null)
+                return;
+
+            Node p1 = start;
+            Node p2 = p1.next;
+            p1.next = null;
+            p1.prev = p2;
+            while (p2 != null )
+            {
+                p2.prev = p2.next;
+                p2.next = p1;
+                p1 = p2;
+                p2 = p2.prev;
+            }
+            start = p1;
+        }
+    }
+}
