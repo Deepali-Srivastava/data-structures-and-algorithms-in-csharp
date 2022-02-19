8e7ea354a6b2ae8a3a878b8dd781c0350fcc7e15
Deepali Srivastava <deepalisriv9@gmail.com>
2022-02-19 17:37:13 +0530

    Header linked list in C#

---
+++ linked-list/HeaderLinkedListProject/Demo.cs
@@ -0,0 +1,73 @@
+﻿/*
+Copyright (C) Deepali Srivastava - All Rights Reserved
+This code is part of DSA course available on CourseGalaxy.com    
+*/
+
+using System;
+
+namespace HeaderLinkedListProject
+{
+    class Demo
+    {
+        static void Main(string[] args)
+        {
+            int choice, data, x, k;
+		    		
+	    	HeaderLinkedList List = new HeaderLinkedList();
+		    List.CreateList();
+		
+		    while(true)
+		    {
+			    Console.WriteLine("1.Display list");
+			    Console.WriteLine("2.Insert a node at the end of the list");
+			    Console.WriteLine("3.Insert a new node before a node");
+			    Console.WriteLine("4.Insert at a given position");
+			    Console.WriteLine("5.Delete a node");
+			    Console.WriteLine("6.Reverse the list");
+			    Console.WriteLine("7.Quit");
+			    Console.Write("Enter your choice : ");
+			    choice=Convert.ToInt32(Console.ReadLine());
+			
+			    if(choice==7)
+				    break;
+
+			    switch(choice)
+			    {
+			     case 1:
+				    List.DisplayList();
+				    break;
+			     case 2:
+				    Console.Write("Enter the element to be inserted : ");
+				    data = Convert.ToInt32(Console.ReadLine());
+				    List.InsertAtEnd(data);
+				    break;
+			     case 3:
+				    Console.Write("Enter the element to be inserted : ");
+				    data = Convert.ToInt32(Console.ReadLine());
+				    Console.Write("Enter the element before which to insert : ");
+				    x = Convert.ToInt32(Console.ReadLine());
+				    List.InsertBefore(data,x);
+				    break;
+			     case 4:
+				    Console.Write("Enter the element to be inserted : ");
+				    data = Convert.ToInt32(Console.ReadLine());
+				    Console.Write("Enter the position at which to insert : ");
+				    k = Convert.ToInt32(Console.ReadLine());
+				    List.InsertAtPosition(data,k);
+				    break;
+			     case 5:
+				    Console.Write("Enter the element to be deleted : ");
+				    data = Convert.ToInt32(Console.ReadLine());
+				    List.DeleteNode(data);	
+				    break;
+			     case 6:
+				    List.Reverse();
+				    break;
+			     default:
+				    Console.WriteLine("Wrong choice");
+                    break;
+			    }
+		  }
+      }
+   }
+}
---
+++ linked-list/HeaderLinkedListProject/HeaderLinkedList.cs
@@ -0,0 +1,136 @@
+﻿/*
+Copyright (C) Deepali Srivastava - All Rights Reserved
+This code is part of DSA course available on CourseGalaxy.com    
+*/
+
+using System;
+
+namespace HeaderLinkedListProject
+{
+    class HeaderLinkedList
+    {
+        private Node head;
+
+        public HeaderLinkedList()
+        {
+            head = new Node(0);
+        }
+
+        public void DisplayList()
+	    {
+		    if ( head.link == null )
+		    {
+			    Console.WriteLine("List is empty\n");
+			    return;
+		    }
+		    Node p = head.link;
+		    Console.Write("List is :\n");
+		    while( p != null )
+		    {
+			    Console.Write(p.info + " ");
+			    p = p.link;
+		    }
+		    Console.WriteLine();
+	    }
+
+        public void InsertAtEnd(int data)
+        {
+            Node temp = new Node(data);
+
+            Node p = head;
+            while (p.link != null)
+                p = p.link;
+
+            p.link = temp;
+            temp.link = null;
+        }
+
+        public void CreateList()
+	    {
+		    int i, n, data;
+		    		
+		    Console.Write("Enter the number of nodes : ");
+		    n = Convert.ToInt32(Console.ReadLine());
+		
+		    for( i = 1; i <= n; i++ )
+		    {
+			    Console.Write("Enter the element to be inserted : ");
+			    data = Convert.ToInt32( Console.ReadLine() );
+			    InsertAtEnd( data );	
+		    }
+	    }
+
+        public void InsertBefore(int data, int x)
+	    {
+		    Node temp;
+		
+		    /*Find pointer to predecessor of node containing x*/
+		    Node p = head;
+		    while( p.link != null )
+		    {
+			    if ( p.link.info == x )
+				    break;
+			    p = p.link;
+		    }
+		
+		    if ( p.link == null )
+			    Console.WriteLine(x + " not present in the list");
+		    else
+		    {
+			    temp = new Node(data);
+			    temp.link = p.link;
+			    p.link = temp;
+		    }
+	    }
+
+        public void InsertAtPosition(int data, int k)
+	    {
+		    Node temp;
+		    int i;
+		
+		    Node p = head;
+		    for( i = 1; i <= k-1 && p != null; i++ )
+			    p = p.link;
+		
+		    if ( p == null )
+			    Console.WriteLine("You can insert only upto " + (i-1) + "th position\n\n");
+		    else
+		    {
+			    temp = new Node(data);
+			    temp.link = p.link;
+			    p.link = temp;
+		    }	
+	    }
+
+        public void DeleteNode(int data)
+	    {
+		    Node p = head;
+		    while(p.link != null)
+		    {
+			    if(p.link.info == data)   
+				    break;
+			    p = p.link;
+		    }
+
+		    if(p.link == null)
+			    Console.WriteLine(data + "Element %d not found");
+		    else
+			    p.link = p.link.link;
+	    }
+
+        public void Reverse()
+        {
+            Node prev, p, next;
+            prev = null;
+            p = head.link;
+            while (p != null)
+            {
+                next = p.link;
+                p.link = prev;
+                prev = p;
+                p = next;
+            }
+            head.link = prev;
+        }
+    }
+}
