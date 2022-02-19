2cae45ee901d52e839154d26da66a5885214440c
Deepali Srivastava <deepalisriv9@gmail.com>
2022-02-19 17:55:56 +0530

    Factorial in C#

---
+++ recursion/Factorial/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+Copyright (C) Deepali Srivastava - All Rights Reserved
+This code is part of DSA course available on CourseGalaxy.com    
+*/
+
+using System;
+
+namespace Factorial
+{
+    class Program
+    {
+        public static long Factorial(int n)
+        {
+            if (n == 0)
+                return 1;
+            return n * Factorial(n - 1);
+        }
+
+        static void Main(string[] args)
+        {
+            int n;
+		    Console.Write("Enter a number greater than or equal to zero : ");
+		    n = Convert.ToInt32(Console.ReadLine());
+		
+		    Console.WriteLine("Factorial of " + n + " is " + Factorial(n));
+        }
+    }
+}
