06b66045ac665f4b189aebf65d7c11a9413c0d4b
Deepali Srivastava <deepalisriv9@gmail.com>
2022-02-19 17:52:16 +0530

    Separate Chaining in C#

---
+++ hashing/SeparateChaining/HashTable.cs
@@ -0,0 +1,80 @@
+﻿/*
+Copyright (C) Deepali Srivastava - All Rights Reserved
+This code is part of DSA course available on CourseGalaxy.com    
+*/
+
+using System;
+
+namespace SeparateChaining
+{
+    public class HashTable
+    {
+        private SingleLinkedList[] array;
+        private int m;  //size of the array
+        private int n; 	//number of records
+
+        public HashTable() : this(11)
+        {
+        }
+
+        public HashTable(int tableSize)
+        {
+            m = tableSize;
+            n = 0;
+            array = new SingleLinkedList[tableSize];
+        }
+
+        int hash(int key)
+        {
+            return (key % m);
+        }
+
+        public void DisplayTable()
+	    {
+		    for(int i=0; i<m; i++)
+		    {
+		       Console.Write( "[" + i + "]  -->  ");
+		   
+		       if(array[i]!= null)
+		      	    array[i].displayList();
+		       else
+			       Console.WriteLine("___");
+		    }
+	    }
+
+        public studentRecord Search(int key)
+        {
+            int h = hash(key);
+
+            if (array[h] != null)
+                return array[h].search(key);
+
+            return null;
+        }
+
+        public void Insert(studentRecord newRecord)
+        {
+            int key = newRecord.getstudentId();
+            int h = hash(key);
+
+            if (array[h] == null)
+                array[h] = new SingleLinkedList();
+
+            array[h].insertInBeginning(newRecord);
+            n++;
+        }
+
+        public void Delete(int key)
+	    {
+		    int h = hash(key);
+
+            if (array[h] != null)
+            {
+                array[h].deleteNode(key);
+                n--;
+            }
+            else
+                Console.WriteLine("Value " + key + " not present\n");
+	    }
+    }
+}
---
+++ hashing/SeparateChaining/Node.cs
@@ -0,0 +1,21 @@
+﻿/*
+Copyright (C) Deepali Srivastava - All Rights Reserved
+This code is part of DSA course available on CourseGalaxy.com    
+*/
+
+using System;
+
+namespace SeparateChaining
+{
+    public class Node
+    {
+        public studentRecord info;
+        public Node link;
+
+        public Node(studentRecord rec)
+        {
+            info = rec;
+            link = null;
+        }
+    }
+}
