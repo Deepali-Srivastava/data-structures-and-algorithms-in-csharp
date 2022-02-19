b993c361b51a883e23e4bb93855d2613d680a36c
Deepali Srivastava <deepalisriv9@gmail.com>
2022-02-19 17:48:40 +0530

    Infix to postfix

---
+++ stack-queue/PostfixProject/Demo.cs
@@ -0,0 +1,132 @@
+﻿/*
+Copyright (C) Deepali Srivastava - All Rights Reserved
+This code is part of DSA course available on CourseGalaxy.com    
+*/
+
+using System;
+
+namespace PostfixProject
+{
+    class Demo
+    {
+        static void Main(string[] args)
+        {
+            String infix;
+			
+		    Console.Write("Enter infix expression : ");
+            infix = Console.ReadLine();
+		
+		    String postfix = infixToPostfix(infix);
+		
+		    Console.WriteLine("Postfix expression is : " + postfix);
+		
+		    Console.WriteLine("Value of expression : " + evaluatePostfix(postfix));
+		}
+        
+        public static String infixToPostfix(String infix)
+        {
+            String postfix = "";
+
+            StackChar st = new StackChar(20);
+
+            char next, symbol;
+
+            for (int i = 0; i < infix.Length; i++)
+            {
+                symbol = infix[i];
+
+                if (symbol == ' ' || symbol == '\t') /*ignore blanks and tabs*/
+                    continue;
+
+                switch (symbol)
+                {
+                    case '(':
+                        st.Push(symbol);
+                        break;
+                    case ')':
+                        while ((next = st.Pop()) != '(')
+                            postfix = postfix + next;
+                        break;
+                    case '+':
+                    case '-':
+                    case '*':
+                    case '/':
+                    case '%':
+                    case '^':
+                        while (!st.IsEmpty() && Precedence(st.Peek()) >= Precedence(symbol))
+                            postfix = postfix + st.Pop();
+                        st.Push(symbol);
+                        break;
+                    default: /*operand*/
+                        postfix = postfix + symbol;
+                        break;
+                }
+            }
+            while (!st.IsEmpty())
+                postfix = postfix + st.Pop();
+            return postfix;
+        }
+
+        public static int Precedence(char symbol)
+        {
+            switch (symbol)
+            {
+                case '(':
+                    return 0;
+                case '+':
+                case '-':
+                    return 1;
+                case '*':
+                case '/':
+                case '%':
+                    return 2;
+                case '^':
+                    return 3;
+                default:
+                    return 0;
+            }
+        }
+
+        public static int evaluatePostfix(String postfix)
+        {
+            StackInt st = new StackInt(20);
+
+            int x, y;
+            for (int i = 0; i < postfix.Length; i++)
+            {
+                if (Char.IsDigit(postfix[i]))
+                    st.Push (Convert.ToInt32(Char.GetNumericValue(postfix[i])));
+                else
+                {
+                    x = st.Pop();
+                    y = st.Pop();
+                    switch (postfix[i])
+                    {
+                        case '+':
+                            st.Push(y + x); break;
+                        case '-':
+                            st.Push(y - x); break;
+                        case '*':
+                            st.Push(y * x); break;
+                        case '/':
+                            st.Push(y / x); break;
+                        case '%':
+                            st.Push(y % x); break;
+                        case '^':
+                            st.Push(power(y, x));
+                            break;
+                    }
+                }
+            }
+            return st.Pop();
+        }
+
+        public static int power(int b, int a)
+        {
+            int i, x = 1;
+            for (i = 1; i <= a; i++)
+                x = x * b;
+            return x;
+        }
+    }
+}
---
+++ stack-queue/PostfixProject/StackInt.cs
@@ -0,0 +1,93 @@
+﻿/*
+Copyright (C) Deepali Srivastava - All Rights Reserved
+This code is part of DSA course available on CourseGalaxy.com    
+*/
+
+using System;
+
+namespace PostfixProject
+{
+    class StackInt
+    {
+        private int[] stackArray;
+        private int top;
+
+        public StackInt()
+        {
+            stackArray = new int[10];
+            top = -1;
+        }
+
+        public StackInt(int maxSize)
+        {
+            stackArray = new int[maxSize];
+            top = -1;
+        }
+
+        public int Size()
+        {
+            return top + 1;
+        }
+
+        public bool IsEmpty()
+        {
+            return (top == -1);
+        }
+
+        public bool IsFull()
+        {
+            return ( top == stackArray.Length - 1 );
+        }
+
+        public void Push(int x)
+        {
+    	    if ( IsFull() )
+    	    {
+    		    Console.WriteLine("Stack Overflow\n");
+    		    return;
+    	    }
+    	    top = top + 1;
+    	    stackArray[top] = x;
+        }
+
+        public int Pop()
+        {
+    	    int x;
+    	    if ( IsEmpty() )
+    	    {
+    		    Console.WriteLine("Stack Underflow\n");
+    		    throw new System.InvalidOperationException();
+    	    }
+    	    x = stackArray[top];
+    	    top = top-1;
+    	    return x;
+        }
+
+        public int Peek()
+        {
+    	    if ( IsEmpty() )
+    	    {
+    		    Console.WriteLine("Stack Underflow\n");
+    		    throw new System.InvalidOperationException();
+    	    }
+    	    return stackArray[top];
+        }
+        
+        public void display()
+        {
+    	    int i;
+    	
+    	    Console.WriteLine("top= " + top);
+
+    	    if ( IsEmpty() )
+    	    {
+    		    Console.WriteLine("Stack is empty");
+    		    return;
+    	    }
+            Console.WriteLine("Stack is : ");
+    	    for ( i = top; i >= 0; i-- )
+    		    Console.WriteLine( stackArray[i] + " " );
+    	    Console.WriteLine();
+        }
+    }
+}
