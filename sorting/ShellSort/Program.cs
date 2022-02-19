01c13b501cab433e3c92ea352217311b42bfda94
Deepali Srivastava <deepalisriv9@gmail.com>
2022-02-19 17:43:09 +0530

    Shell Sort in C#

---
+++ sorting/ShellSort/Program.cs
@@ -0,0 +1,54 @@
+﻿/*
+Copyright (C) Deepali Srivastava - All Rights Reserved
+This code is part of DSA course available on CourseGalaxy.com    
+*/
+
+using System;
+
+namespace ShellSort
+{
+        class Program
+        {
+            public static void Sort(int[] a, int n)
+            {
+                int i, j, temp, h;
+		    		   
+		        Console.Write("Enter maximum increment(odd value) : ");
+		        h = Convert.ToInt32( Console.ReadLine() );
+		
+		        while ( h >= 1 )
+		        {
+			        for (i = h; i < n; i++)
+			        {
+				        temp = a[i];
+				        for (j = i-h; j>=0 && a[j]>temp; j = j-h)
+					        a[j+h] = a[j];
+				        a[j+h] = temp;
+			        }
+			        h = h-2; 
+		        }
+             }
+        
+            static void Main(string[] args)
+            {
+                int i, n;
+                int[] a = new int[20];
+
+                Console.Write("Enter the number of elements : ");
+                n = Convert.ToInt32(Console.ReadLine());
+
+                for (i = 0; i < n; i++)
+                {
+                    Console.Write("Enter element " + (i + 1) + " : ");
+                    a[i] = Convert.ToInt32(Console.ReadLine());
+                }
+
+                Sort(a, n);
+
+                Console.WriteLine("Sorted array is : ");
+                for (i = 0; i < n; i++)
+                    Console.Write(a[i] + " ");
+                Console.WriteLine();
+            }
+      }
+}
