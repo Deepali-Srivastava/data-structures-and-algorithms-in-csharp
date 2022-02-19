978819499999bd0f3da598ae796a8ed13ab9cb70
Deepali Srivastava <deepalisriv9@gmail.com>
2022-02-19 17:49:25 +0530

    Checking nested parentheses

---
+++ stack-queue/ParenthesesProject/Demo.cs
@@ -0,0 +1,77 @@
+﻿/*
+Copyright (C) Deepali Srivastava - All Rights Reserved
+This code is part of DSA course available on CourseGalaxy.com    
+*/
+
+using System;
+
+namespace ParenthesesProject
+{
+    class Demo
+    {
+        static void Main(string[] args)
+        {
+            String expression;
+
+	        Console.Write("Enter an expression with parentheses : ");
+            expression = Console.ReadLine();
+		
+	        if ( IsValid(expression) )
+		        Console.WriteLine("Valid expression");
+	        else
+		        Console.WriteLine("Invalid expression");
+	    }
+
+        static bool IsValid(String expr)
+	    {
+	  	    StackA st = new StackA();
+
+	  	    char ch;
+	  	    
+            for ( int i = 0; i < expr.Length; i++)
+	  	    {
+	  		    if ( expr[i] == '(' || expr[i] == '{' || expr[i] == '[' )
+	  		    	st.Push(expr[i]);
+
+	  		    if ( expr[i] == ')' || expr[i] == '}' || expr[i] == ']')
+	  			    if ( st.IsEmpty() )    
+	  			    {
+	  				    Console.WriteLine("Right parentheses are more than left parentheses");
+	  				    return false;
+	  			    }
+	  			    else
+	  			    {
+	  				    ch = st.Pop();
+	  				    if ( !MatchParentheses(ch,expr[i]) ) 
+	  				    {
+	  					    Console.WriteLine("Mismatched parentheses are : "); 
+	  					    Console.WriteLine(ch + " and " + expr[i]);	
+	  					    return false;
+	  				    }
+	  			    }
+	  	    }
+
+	  	    if ( st.IsEmpty() ) 
+	  	    {
+	  		    Console.WriteLine("Balanced Parentheses"); 
+	  		    return true;
+	  	    }
+	  	    else 
+	  	    {
+	  		    Console.WriteLine("Left parentheses are more than right parentheses");	
+	  		    return false;
+	  	    }
+	  	}
+
+        static bool MatchParentheses(char leftPar, char rightPar)
+        {
+            if ( leftPar == '[' && rightPar == ']' )
+                return true;
+            if ( leftPar == '{' && rightPar == '}' )
+                return true;
+            if ( leftPar == '(' && rightPar == ')' )
+                return true;
+            return false;
+        }
+    }
+}
---
+++ stack-queue/ParenthesesProject/StackA.cs
@@ -0,0 +1,93 @@
+﻿/*
+Copyright (C) Deepali Srivastava - All Rights Reserved
+This code is part of DSA course available on CourseGalaxy.com    
+*/
+
+using System;
+
+namespace ParenthesesProject
+{
+    class StackA
+    {
+        private char[] stackArray;
+        private int top;
+
+        public StackA()
+        {
+            stackArray = new char[10];
+            top = -1;
+        }
+
+        public StackA(int maxSize)
+        {
+            stackArray = new char[maxSize];
+            top = -1;
+        }
+
+        public int Size()
+        {
+            return top + 1;
+        }
+
+        public bool IsEmpty()
+        {
+            return ( top == -1 );
+        }
+
+        public bool IsFull()
+        {
+            return ( top == stackArray.Length - 1 );
+        }
+
+        public void Push(char x)
+        {
+    	    if( IsFull() )
+    	    {
+    		    Console.WriteLine("Stack Overflow\n");
+    		    return;
+    	    }
+    	    top = top+1;
+    	    stackArray[top] = x;
+        }
+
+        public char Pop()
+        {
+    	    char x;
+    	    if( IsEmpty() )
+    	    {
+    		    Console.WriteLine("Stack Underflow\n");
+    		    throw new System.InvalidOperationException();
+    	    }
+    	    x = stackArray[top];
+    	    top = top-1;
+    	    return x;
+        }
+
+        public char peek()
+        {
+    	    if ( IsEmpty() )
+    	    {
+    		    Console.WriteLine("Stack Underflow\n");
+    		    throw new System.InvalidOperationException();
+    	    }
+    	    return stackArray[top];
+        }
+        
+        public void Display()
+        {
+    	    int i;
+    	
+    	    Console.WriteLine("top= " + top);
+
+    	    if ( IsEmpty() )
+    	    {
+    		    Console.WriteLine("Stack is empty");
+    		    return;
+    	    }
+            Console.WriteLine("Stack is : ");
+    	    for ( i = top; i >= 0; i-- )
+    		    Console.WriteLine(stackArray[i] + " ");
+    	    Console.WriteLine();
+        }
+    }
+}
