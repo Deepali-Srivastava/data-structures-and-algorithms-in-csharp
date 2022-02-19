7e0442ba37cbb67a0ed0fddc443651dc711e5ef4
Deepali Srivastava <deepalisriv9@gmail.com>
2022-02-19 17:56:42 +0530

    Euclid’s algorithm in C#

---
+++ recursion/Euclids/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+Copyright (C) Deepali Srivastava - All Rights Reserved
+This code is part of DSA course available on CourseGalaxy.com    
+*/
+
+using System;
+
+namespace Euclids
+{
+    class Program
+    {
+        public static int GCD(int a, int b)
+        {
+            if (b == 0)
+                return a;
+            
+            return GCD(b, a % b);
+        }
+
+        static void Main(string[] args)
+        {
+            int a,b;
+
+		    Console.Write("Enter values for a and b : ");
+		    a = Convert.ToInt32(Console.ReadLine());
+		    b = Convert.ToInt32(Console.ReadLine());
+		    
+            Console.WriteLine("GCD of " + a + " and " + b + " is " + GCD(a,b));
+        }
+    }
+}
