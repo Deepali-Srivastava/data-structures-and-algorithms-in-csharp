0710cebeaffa0c298b3cf97b3224de7f2055b826
Deepali Srivastava <deepalisriv9@gmail.com>
2022-02-19 17:55:31 +0530

    Fibonacci in C#

---
+++ recursion/Fibonacci/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+Copyright (C) Deepali Srivastava - All Rights Reserved
+This code is part of DSA course available on CourseGalaxy.com    
+*/
+
+using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        public static int Fib(int n)
+        {
+            if (n == 0)
+                return 0;
+            if (n == 1)
+                return 1;
+            return Fib(n - 1) + Fib(n - 2);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number of terms : ");
+		    int terms = Convert.ToInt32(Console.ReadLine());
+		
+		    for( int i = 0; i <= terms; i++ )
+                Console.Write(Fib(i)+" ");
+        }
+    }
+}
